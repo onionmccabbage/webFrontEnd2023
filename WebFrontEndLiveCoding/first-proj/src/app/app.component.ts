5901e680cca9fa40038735816ed1324ba41742af
onionmccabbage <onionmccabbage@live.com>
2023-08-22 20:38:39 +0100

    just before last lab on day 2

--- WebFrontEndLiveCoding/first-proj/src/app/app.component.ts
+++ WebFrontEndLiveCoding/first-proj/src/app/app.component.ts
@@ -13,6 +13,16 @@ export class AppComponent {
   a = 'a kitten'
   t = 'picture of a kitten'
   catName='Mildred'
+  // here is a portfolio array
+  portfolio = [
+    {'ticker':'AAPL', 'qty':10, 'price':100},
+    {'ticker':'AMAZN', 'qty':20, 'price':200},
+    {'ticker':'C', 'qty':30, 'price':300},
+    {'ticker':'FB', 'qty':40, 'price':400},
+    {'ticker':'TSLA', 'qty':50, 'price':500}
+  ]
+
+
   // methods of this class
   handleButton(){
     // in Angular, 'this' refers the the curent class
--- WebFrontEndLiveCoding/first-proj/src/app/portfolio/portfolio.component.ts
+++ WebFrontEndLiveCoding/first-proj/src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
@@ -7,11 +7,6 @@ import { Component } from '@angular/core';
 })
 export class PortfolioComponent {
   // properties
-  portfolio = [
-    {'ticker':'AAPL', 'qty':10, 'price':100},
-    {'ticker':'AMAZN', 'qty':20, 'price':200},
-    {'ticker':'C', 'qty':30, 'price':300},
-    {'ticker':'FB', 'qty':40, 'price':400},
-    {'ticker':'TSLA', 'qty':50, 'price':500}
-  ]
+  // @Input() lets us inject proeprties from the parent component
+  @Input() portfolio:any // its a good idea to give it a Typescript type
 }
