74d8197f68e9556384f071869d423f38f008514f
onionmccabbage <onionmccabbage@live.com>
2023-08-23 16:01:38 +0100

    coffee break day 3

--- WebFrontEndLiveCoding/use-api-proj/src/app/app.component.ts
+++ WebFrontEndLiveCoding/use-api-proj/src/app/app.component.ts
@@ -13,12 +13,13 @@ export class AppComponent {
   constructor(private typicode:TypicodeService){}
 
   // when the component first loads, it will run ngOnInit()
-  ngOnInit(){
+  ngOnInit(){ // 
     // make a call to our 'getAllPhotos' service
     this.typicode.getAllPhotos() // remember -this is an Observable
       .subscribe( this.handleAllPhotos() ) // we can subscribe to an Observable
   }
   handleAllPhotos(){
+    // received is whatever data eventually comes back from the server
     return (received:any)=>{
       this.photos = received
     }
