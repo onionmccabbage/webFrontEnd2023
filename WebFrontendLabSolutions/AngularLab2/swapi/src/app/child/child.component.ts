7f7c944e9a391e990980e689b7c5e5a972cc7ffa
onionmccabbage <onionmccabbage@live.com>
2023-08-24 19:31:33 +0100

    sequential services

--- WebFrontendLabSolutions/AngularLab2/swapi/src/app/child/child.component.ts
+++ WebFrontendLabSolutions/AngularLab2/swapi/src/app/child/child.component.ts
@@ -17,19 +17,37 @@ export class ChildComponent implements OnInit {
   statusObj: StatusObj = new StatusObj('Received OK')
   swapiReport: any = {}
   filmDetails: any = {}
-
+  
   doEmitEvent() {
     this.SWAPI_EVENT.emit(this.statusObj);
   }
   constructor(private getSwapiService: GetSwapiService) { }
-
+  
   getSwapiData() {
     this.getSwapiService.getSwapiReport(this.category, this.itemNum)
-      .subscribe(
-        (swapiReport) => {
-          console.log(this.category)
-          this.swapiReport = swapiReport
-          this.doEmitEvent()
+    .subscribe(
+      (swapiReport) => {
+        console.log(this.category)
+        this.swapiReport = swapiReport
+        // iterate over the data returned from the FIRST service call
+        for (let film in this.swapiReport['films']) {
+          console.log(this.swapiReport['films'][film])
+            // make a call to get the details of each film...
+            let filmUrl = `https://swapi.dev/api/films/${film}`
+            if (Number(film) >0) { // ensure we dont accidentally get film=0
+              this.getSwapiService.getRelatedFilmData(filmUrl)
+              .subscribe(
+                (returnedFilmDetails:any) => {
+                  // we need an empty array for the results of the SECOND service call
+                  let filmArr = []
+                  console.log(returnedFilmDetails['title'])
+                  filmArr.push(returnedFilmDetails)
+                  // then you make use of this filmArray however you like
+                }
+                )
+                this.doEmitEvent()
+              }
+            }
         }, error => {
           this.statusObj.status = `Could not find item ${this.itemNum} in category ${this.category}`
           this.swapiReport = {}
@@ -38,7 +56,7 @@ export class ChildComponent implements OnInit {
       )
   }
 
-  getFilmDetails(paramFilmUrl:string) {
+  getFilmDetails(paramFilmUrl: string) {
     console.log(paramFilmUrl)
     // event.preventDefault()
     this.getSwapiService.getRelatedFilmData(paramFilmUrl)
